adbf6c0b837c2c98e792d8354f6d322006e46cff
vishal <vishalvidhate196@gmail.com>
2023-12-10 15:29:32 +0530

    second_comment

---
+++ app/src/main/java/com/mumbaitest/data/depend.kt
@@ -0,0 +1,15 @@
+package com.mumbaitest.data
+
+object depend {
+    //Retrofit
+  /*  implementation ("com.google.code.gson:gson:2.9.0")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
+    implementation ("com.squareup.okhttp3:logging-interceptor:4.10.0")
+
+    //Corutine
+    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-android:1.7.1")
+
+    //ViewModelScope
+    implementation ("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.2")*/
+}
